fixed new api for gradient boosting

PrimalCore/python/PrimalCore/models/base.py
@@ -36,7 +36,7 @@ __author__ = "Andrea Tramacere"
 # eg numpy 
 # absolute import eg: import numpy as np
 import  numpy as np
-
+from sklearn.ensemble import GradientBoostingRegressor
 # Project
 # relative import eg: from .mod import f
 from ..homogeneous_table.dataset_handler import check_dataset_decorate
@@ -164,8 +164,12 @@ class BaseModel(object):
         if hasattr(self.clf, 'estimators_'):
             _preds = np.zeros((features.shape[0],len(self.clf.estimators_)))
 
-        for i in range(len(self.clf.estimators_)):
-            _preds[:,i]=self.clf.estimators_[i].predict(features)
+        if isinstance(self.clf,GradientBoostingRegressor):
+            for i in range(len(self.clf.estimators_.reshape(-1))):
+              _preds[:,i]=self.clf.estimators_.reshape(-1)[i].predict(features)
+        else:
+            for i in range(len(self.clf.estimators_)):
+              _preds[:,i]=self.clf.estimators_[i].predict(features)
 
         return _preds
 

PrimalCore/python/PrimalCore/models/regression.py
@@ -131,14 +131,14 @@ class Regressor(BaseModel):
         """
         n_estimators_range = np.arange(50, 500, 50)
         param_grid = {'n_estimators': n_estimators_range}
-        clf=GradientBoostingRegressor(n_estimators=n_estimators,
+
+        return cls(GradientBoostingRegressor(n_estimators=n_estimators,
                                              learning_rate=learning_rate,
                                              loss='huber',
                                              alpha=0.01,
                                              subsample=0.8,
                                              min_samples_leaf=10,
-                                             min_samples_split=10),
-        return cls(clf,par_grid_dict=param_grid,name=name)
+                                             min_samples_split=10),par_grid_dict=param_grid,name=name)
 
     @classmethod
     def ABRegressor(cls, n_estimators=100,name='ada_boost'):