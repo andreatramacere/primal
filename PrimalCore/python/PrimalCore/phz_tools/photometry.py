de0dd3887cf4408d7ccd8a1ed7f5aeb76e85e4a6
Florian Dubath <florian.dubath@unige.ch>
2020-03-11 11:31:49 +0100

    Add the ASINH MAG & Color computation from Lupton99

--- PrimalCore/python/PrimalCore/phz_tools/photometry.py
+++ PrimalCore/python/PrimalCore/phz_tools/photometry.py
@@ -8,7 +8,10 @@ general info about this module
 Summary
 ---------
 .. autosummary::
+   Mag
+   AsinhMag
    Color
+   AsinhColor
    FluxRatio
 
 Module API
@@ -36,6 +39,8 @@ from __future__ import absolute_import, division, print_function
 
 from builtins import (bytes, str, open, super, range,
                       zip, round, input, int, pow, object, map, zip)
+                      
+from numpy import log10, log, arcsinh
 
 __author__ = "Andrea Tramacere"
 
@@ -50,7 +55,69 @@ __author__ = "Andrea Tramacere"
 # Project
 # relative import eg: from .mod import f
 
+      
+class Mag(object):
+    """
+    Class to build a feature array of magnitude from a flux feature array
 
+    Parameters
+    ----------
+    name : string
+        name of the new feature
+    band_1 : sting
+        name of the input feature
+    zero_point: float
+        flux zero-point, by default 3.631E9 : AB mag for flux in mJy
+    features : optional, :class:`PrimalCore.python.PrimalCore.homogeneous_table.dataset.MLDataSet` object
+
+    """
+    def __init__(self,name,band_1,zero_point=3.631E9,features=None):
+
+        self.name=name
+        self.band_1=band_1
+        self.zero_point=zero_point
+
+        if features is not None:
+            self.values=self.build(features,self.band_1,self.zero_point)
+
+
+    def build(self,features,band_1,zero_point):
+        return -2.5*log10(features.get_feature_by_name(band_1)/zero_point) 
+
+
+class AsinhMag(object):
+    """
+    Class to build a feature array of asinh modified magnitude from a flux feature array
+    See Lupton 99 (https://arxiv.org/pdf/astro-ph/9903081.pdf)
+
+    Parameters
+    ----------
+    name : string
+        name of the new feature
+    band_1 : sting
+        name of the input feature
+    b:float
+        softening parameter
+    zero_point: float
+        flux zero-point, by default 3.631E9 : AB mag for flux in mJy
+
+    features : optional, :class:`PrimalCore.python.PrimalCore.homogeneous_table.dataset.MLDataSet` object
+
+    """
+    def __init__(self,name,band_1,b,zero_point=3.631E9,features=None):
+
+        self.name=name
+        self.band_1=band_1
+        self.b=b
+        self.zero_point=zero_point
+
+        if features is not None:
+            self.values=self.build(features,self.band_1,self.b,self.zero_point)
+
+
+    def build(self,features,band_1,b,zero_point):
+        a = 1.0857362 # Pogson ratio = 2.5*log10(e)
+        return -a*(arcsinh(features.get_feature_by_name(band_1)/(zero_point*2.0*b))+log(b)) 
 
 class Color(object):
     """
@@ -79,7 +146,44 @@ class Color(object):
 
     def build(self,features,band_1,band_2):
         return features.get_feature_by_name(band_2)-features.get_feature_by_name(band_1)
+        
+class AsinhColor(object):
+    """
+    Class to build a feature array of Asinh colors from two flux features arrays
+    See Lupton 99 (https://arxiv.org/pdf/astro-ph/9903081.pdf)
 
+    Parameters
+    ----------
+    name : string
+        name of the new feature
+    band_1 : sting
+        name of the input feature
+    band_2 :
+        name of the input feature
+    b:float
+        softening parameter
+    zero_point: float
+        flux zero-point, by default 3.631E9, : AB mag for flux in mJy
+    features : optional, :class:`PrimalCore.python.PrimalCore.homogeneous_table.dataset.MLDataSet` object
+
+    """
+    def __init__(self,name,band_1,band_2,b,zero_point=3.631E9,features=None):
+
+        self.name=name
+        self.band_1=band_1
+        self.band_2=band_2
+        self.b=b
+        self.zero_point=zero_point
+
+        if features is not None:
+            self.values=self.build(features,self.band_1,self.band_2,self.b,self.zero_point)
+
+
+    def build(self,features,band_1,band_2,b,zero_point):
+        a = 1.0857362 # Pogson ratio = 2.5*log10(e)
+        return -a*(arcsinh(features.get_feature_by_name(band_2)/(zero_point*2.0*b)) - arcsinh(features.get_feature_by_name(band_1)/(zero_point*2.0*b))) 
+        
+        
 
 class FluxRatio(object):
     """
